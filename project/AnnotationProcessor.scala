2f2ee06939c61b9092719028b84fc89e838dc347
kelebra <kelebra20@gmail.com>
2018-04-06 21:00:35 -0500

    Better spacing

--- project/AnnotationProcessor.scala
+++ project/AnnotationProcessor.scala
@@ -53,9 +53,9 @@ object AnnotationProcessor {
 
   private def readme(percent: Int) =
     "## Project status:\n" +
-      s"""![Progress](http://progressed.io/bar/$percent)""" +
-      """[![Build Status](https://travis-ci.org/kelebra/s-99.svg?branch=master)](https://travis-ci.org/kelebra/s-99)""" +
-      """[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=kelebra/s-99&amp;utm_campaign=Badge_Grade)""" +
+      s"""![Progress](http://progressed.io/bar/$percent) """ +
+      """[![Build Status](https://travis-ci.org/kelebra/s-99.svg?branch=master)](https://travis-ci.org/kelebra/s-99) """ +
+      """[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=kelebra/s-99&amp;utm_campaign=Badge_Grade) """ +
       """[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&utm_medium=referral&utm_content=kelebra/s-99&utm_campaign=Badge_Coverage)""" +
       """
         |# Solutions to [Scala 99](http://aperiodic.net/phil/scala/s-99/) Problems:
