7b861af4e8d62aa47e069c78f5ec14e5c25e4f12
kelebra <kelebra20@gmail.com>
2018-04-06 20:49:52 -0500

    Simplified tut generation

--- project/AnnotationProcessor.scala
+++ project/AnnotationProcessor.scala
@@ -49,12 +49,10 @@ object AnnotationProcessor {
   private val solutionPattern = """@Solution\(number = ([0-9]+), input = \"(.*)\"\)\nobject ([a-zA-Z]+)""".r
 
   private val readme =
-    """
-      |## Project status:
-      |[![Build Status](https://travis-ci.org/kelebra/s-99.svg?branch=master)](https://travis-ci.org/kelebra/s-99)""" +
+    "## Project status:\n" +
+      """[![Build Status](https://travis-ci.org/kelebra/s-99.svg?branch=master)](https://travis-ci.org/kelebra/s-99)""" +
       """[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=kelebra/s-99&amp;utm_campaign=Badge_Grade)""" +
-      """[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&utm_medium=referral&utm_content=kelebra/s-99&utm_campaign=Badge_Coverage)"""
-        .stripMargin +
+      """[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&utm_medium=referral&utm_content=kelebra/s-99&utm_campaign=Badge_Coverage)""" +
       """
         |# Solutions to [Scala 99](http://aperiodic.net/phil/scala/s-99/) Problems:
         |
