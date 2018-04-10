d24bec1b18bd7052a4d6def36e483fa4fe099a4c
kelebra <kelebra20@gmail.com>
2018-04-10 18:22:25 -0500

    Fixed heading

--- project/AnnotationProcessor.scala
+++ project/AnnotationProcessor.scala
@@ -58,7 +58,7 @@ object AnnotationProcessor {
       """[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=kelebra/s-99&amp;utm_campaign=Badge_Grade) """ +
       """[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/1d6879f769b14cb6be581d36fe5f3897)](https://www.codacy.com/app/kelebra20/s-99?utm_source=github.com&utm_medium=referral&utm_content=kelebra/s-99&utm_campaign=Badge_Coverage)""" +
       """
-        |# Solutions to [Scala 99](http://aperiodic.net/phil/scala/s-99/) Problems:
+        |# Solutions to [Scala 99](http://aperiodic.net/phil/scala/s-99/) Problems
         |
         |```tut
         |import com.github.kelebra.s99.solutions._
