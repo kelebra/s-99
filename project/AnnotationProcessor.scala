e99d177ef85c3867d769eca70ef64ff3d8cac4d3
kelebra <kelebra20@gmail.com>
2018-04-10 18:19:01 -0500

    Simplified naming of problems and enumeration

--- project/AnnotationProcessor.scala
+++ project/AnnotationProcessor.scala
@@ -34,7 +34,7 @@ object AnnotationProcessor {
         .map {
           case (number, problem, application) ⇒
             s"""
-               |## #$number: $problem
+               |$number. ${problem.capitalize}
                |```tut
                |$application
                |```
